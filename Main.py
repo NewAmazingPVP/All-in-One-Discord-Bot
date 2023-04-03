a3f754ece73dc0463078967ee241fa99a00fd625
NewAmazingPVP <gamingofpat@gmail.com>
2023-04-03 14:01:10 -0400

    Initial commit

---
+++ Main.py
@@ -0,0 +1,49 @@
+import discord
+import requests
+from datetime import datetime
+import Key
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('weather'):
+        try:
+            # get city name from user input
+            city = message.content.split()[1]
+
+            # set up API request
+            url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={Key.API_KEY}"
+            response = requests.get(url)
+            data = response.json()
+
+            # extract weather data
+            weather = data['weather'][0]['description']
+            temp = data['main']['temp'] - 273.15
+            humidity = data['main']['humidity']
+            wind = data['wind']['speed']
+            sunrise = data['sys']['sunrise']
+
+            # format sunrise time
+            sunrise_time = datetime.fromtimestamp(sunrise).strftime("%H:%M")
+
+            # create output message
+            output = f"Weather in {city}: {weather}\nTemperature: {temp:.2f}°C\nHumidity: {humidity}%\nWind Speed: {wind}m/s\nSunrise: {sunrise_time} EST"
+
+            # send message to discord
+            await message.channel.send(output)
+
+        except IndexError:
+            # if user did not enter a city name
+            await message.channel.send("Please enter a city name after the /weather command.")
+
+client.run(Key.TOKEN)
