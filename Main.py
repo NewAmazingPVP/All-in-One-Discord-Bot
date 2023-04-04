d574d8e1336cc381bbd71cbd38affbc2e93f87c6
NewAmazingPVP <gamingofpat@gmail.com>
2023-04-04 16:00:57 -0400

    Slash commands semi-implemented
    Slash commands semi-implemented

--- Main.py
+++ Main.py
@@ -1,45 +1,39 @@
-import discord
-import requests
 from datetime import datetime
+import discord
+from discord import app_commands
+import requests
 import Key
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@tree.command(name="weather", description="My first application Command", guild=discord.Object(id=952705219739918416))
+async def first_command(ctx, city: str):
+
+    # API request
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={Key.API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+
+    # extract weather data
+    weather = data['weather'][0]['description']
+    temp = data['main']['temp'] - 273.15
+    humidity = data['main']['humidity']
+    wind = data['wind']['speed']
+    sunrise = data['sys']['sunrise']
+
+    # format sunrise time
+    sunrise_time = datetime.fromtimestamp(sunrise).strftime("%H:%M")
+
+    # create output message
+    output = f"Weather in {city}: {weather}\nTemperature: {temp:.2f}°C\nHumidity: {humidity}%\nWind Speed: {wind}m/s\nSunrise: {sunrise_time} EST"
+
+    await ctx.channel.send(output)
 
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
-    
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('weather'):
-        # get city name from user input
-        city = message.content.split()[1]
-
-        # API request
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={Key.API_KEY}"
-        response = requests.get(url)
-        data = response.json()
-
-        # extract weather data
-        weather = data['weather'][0]['description']
-        temp = data['main']['temp'] - 273.15
-        humidity = data['main']['humidity']
-        wind = data['wind']['speed']
-        sunrise = data['sys']['sunrise']
-
-        # format sunrise time
-        sunrise_time = datetime.fromtimestamp(sunrise).strftime("%H:%M")
-
-        # create output message
-        output = f"Weather in {city}: {weather}\nTemperature: {temp:.2f}°C\nHumidity: {humidity}%\nWind Speed: {wind}m/s\nSunrise: {sunrise_time} EST"
-
-        # send message to discord
-        await message.channel.send(output)
-
+    await tree.sync(guild=discord.Object(id=952705219739918416))
+    print("Ready!")
 
 client.run(Key.TOKEN)
