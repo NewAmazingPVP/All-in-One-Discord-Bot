fb2000fe381f8e3f756d113232be1af43875cdd6
NewAmazingPVP <gamingofpat@gmail.com>
2023-04-07 10:41:32 -0400

    Image generation basics added
    Image generation basics added

--- Main.py
+++ Main.py
@@ -3,10 +3,17 @@ import discord
 from discord import app_commands
 import requests
 import Key
+import json
+import requests
+import io
+import base64
+from PIL import Image, PngImagePlugin
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
+
+url = "http://127.0.0.1:7860"
 
 @tree.command(name="weather", description="Get weather of any city!")
 async def first_command(ctx, city: str):
@@ -30,6 +37,37 @@ async def first_command(ctx, city: str):
     output = f"Weather in {city}: {weather}\nTemperature: {temp:.2f}°C\nHumidity: {humidity}%\nWind Speed: {wind}m/s\nSunrise: {sunrise_time} EST"
 
     await ctx.response.send_message(output)
+
+@tree.command(name="art", description="Draw anything you imagine!")
+async def second_command(ctx, art: str):
+                
+    payload = {
+        "prompt": art,
+        "steps": 25,
+        "negative_prompt": Key.NEGATIVE_PROMPT
+    }
+    response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
+
+
+    r = response.json()
+
+    for i in r['images']:
+        image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+
+        png_payload = {
+        "image": "data:image/png;base64," + i
+        }
+        response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+
+        pnginfo = PngImagePlugin.PngInfo()
+        pnginfo.add_text("parameters", response2.json().get("info"))
+        image.save('output.png', pnginfo=pnginfo)
+    
+    with open('output.png', 'rb') as f:
+        file = discord.File(f, filename='output.png')    
+    
+    await ctx.response.send_message(file=file)    
+
 
 @client.event
 async def on_ready():
